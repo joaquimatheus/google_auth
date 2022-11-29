2be7a519ad3c385ba33d5ef4355a289fa4faf9af
Matheus Zakken <matheus@marcogabriel.com.br>
2022-11-29 16:59:57 -0300

    Created the functionality to choose the theme, put the headeer and more things and styles

--- ui/web/src/components/App/index.jsx
+++ ui/web/src/components/App/index.jsx
@@ -1,20 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
-import { GlobalStyles } from './globalStyles'
-import { StyledApp } from './styles'
+import { GlobalStyles } from '../../themes/globalStyles'
+import { StyledApp, Nav } from './styles'
+
+import { getFromLocalStorage, setToLocalStorage } from '../../helpers/storage'
+import { defaultThemes } from '../../themes/themes'
+import { useTheme } from '../../themes/useTheme'
 
 import Login from '../../pages/Login'
 
+import ThemePicker from '../ThemePicker'
+
 function App() {
+    const localThemes = getFromLocalStorage("all-themes")
+    
+    if (!localThemes) {
+        setToLocalStorage("all-themes", defaultThemes)
+    }
+
+    console.log(defaultThemes)
+
+    const { theme, themeLoaded } = useTheme()
+    const [selectedTheme, setSelectedTheme] = useState(theme)
+
+    useEffect(() => {
+        setSelectedTheme(theme)
+    }, [theme, themeLoaded])
+    
+    console.log('selectedTheme', selectedTheme)
+
     return (
         <>
-            <StyledApp>
-            <GlobalStyles />
-                <Routes>
-                    <Route path="/" element={ <Login /> }/>
-                </Routes>
-            </StyledApp>
+            {themeLoaded && (
+                <ThemeProvider theme={selectedTheme} >
+                    <GlobalStyles />
+                    <StyledApp>
+                        <header>
+                            <Nav>
+                                <ThemePicker themeSetter={setSelectedTheme} />
+                            </Nav>
+                        </header>
+                        <Routes>
+                            <Route path="/" element={ <Login /> }/>
+                        </Routes>
+                    </StyledApp>
+                </ThemeProvider >
+            )}
         </>
     )
 }
--- ui/web/src/components/App/styles.jsx
+++ ui/web/src/components/App/styles.jsx
@@ -10,3 +10,22 @@ export const StyledApp = styled.div`
         padding: 20px;
     }
 `
+
+
+export const Nav = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: flex-end;
+    gap: 20px;
+`
+
+export const NavLinks = styled.li`
+    height: 40px;
+    padding: 10px 20px;
+    background-color: var(--primary);
+    --switch: calc((var(--primary-val) - var(--threshold)) * -100%);
+    color: hsl(0, 0%, var(--switch));
+    border-radius: 6px;
+    cursor: pointer;
+    transition: all 0.4s ease;
+`
