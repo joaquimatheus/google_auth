18aa3b465d7c9c05be3ef011aee9562ac3fcb23c
Matheus Zakken <matheus@marcogabriel.com.br>
2022-11-29 17:00:26 -0300

    global styles fix some errrors

--- ui/web/src/themes/globalStyles.jsx
+++ ui/web/src/themes/globalStyles.jsx
@@ -1,7 +1,7 @@
-import { createGlobalStyle, ThemeProps } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-    :root,
+      :root,
     ::after,
     ::before {
         --bg-first-hue: ${({ theme }) => theme.colors.background.first.h};
@@ -20,49 +20,49 @@ export const GlobalStyles = createGlobalStyle`
             var(--bg-first-hue),
             calc(var(--bg-first-sat) * 1%),
             calc(var(--bg-first-val) * 1%)
-        );
-        --bg-second: hsl(
-            var(--bg-second-hue),
-            calc(var(--bg-second-sat) * 1%),
-            calc(var(--bg-second-val) * 1%)
-        );
-        --primary: hsl(
+    );
+    --bg-second: hsl(
+        var(--bg-second-hue),
+        calc(var(--bg-second-sat) * 1%),
+        calc(var(--bg-second-val) * 1%)
+    );
+    --primary: hsl(
+        var(--primary-hue),
+        calc(var(--primary-sat) * 1%),
+        calc(var(--primary-val) * 1%)
+    );
+    --secondary: hsl(
+        var(--secondary-hue),
+        calc(var(--secondary-sat) * 1%),
+        calc(var(--secondary-val) * 1%)
+    );
+    /* the threshold at which colors are considered "light." */
+        --threshold: 60;
+        --border-threshold: 80;
+    }
+    *,
+    *::after,
+    *::before {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+    ::-webkit-scrollbar-track {
+        background: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+        border-radius: 20px;
+        background-color: hsla(
             var(--primary-hue),
             calc(var(--primary-sat) * 1%),
-            calc(var(--primary-val) * 1%)
-        );
-        --secondary: hsl(
-            var(--secondary-hue),
-            calc(var(--secondary-sat) * 1%),
-            calc(var(--secondary-val) * 1%)
-        );
-        /* the threshold at which colors are considered "light." */
-            --threshold: 60;
-            --border-threshold: 80;
-        }
-        *,
-        *::after,
-        *::before {
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-        }
-        ::-webkit-scrollbar {
-            width: 10px;
-        }
-        ::-webkit-scrollbar-track {
-            background: transparent;
-        }
-        ::-webkit-scrollbar-thumb {
-            border-radius: 20px;
-            background-color: hsla(
-                var(--primary-hue),
-                calc(var(--primary-sat) * 1%),
-                calc(var(--primary-val) * 1%),
-                30%
-        );
-        }
-        body {
-            font-size: 14px;
-        }
+            calc(var(--primary-val) * 1%),
+            30%
+    );
+    }
+    body {
+        font-size: 14px;
+    }
 `
