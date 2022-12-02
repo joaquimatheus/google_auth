27d8bb695d29613dd572427e690f9d1ed3999054
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-01 21:20:35 -0300

    used prettier and added new route /sign-up

--- ui/web/src/components/App/index.jsx
+++ ui/web/src/components/App/index.jsx
@@ -1,58 +1,62 @@
-import { useState, useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-import { GlobalStyles } from '../../themes/globalStyles'
-import { StyledApp, Nav } from './styles'
+import { GlobalStyles } from "../../themes/globalStyles";
+import { StyledApp, Nav } from "./styles";
 
-import { getFromLocalStorage, setToLocalStorage } from '../../helpers/storage'
-import { defaultThemes } from '../../themes/themes'
-import { useTheme } from '../../themes/useTheme'
+import { getFromLocalStorage, setToLocalStorage } from "../../helpers/storage";
+import { defaultThemes } from "../../themes/themes";
+import { useTheme } from "../../themes/useTheme";
 
-import Login from '../../pages/Login'
+import Login from "../../pages/Login";
+import Register from '../../pages/Register';
 
-import ThemePicker from '../ThemePicker'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import ThemePicker from "../ThemePicker";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-    const localThemes = getFromLocalStorage("all-themes")
-    
+    const localThemes = getFromLocalStorage("all-themes");
+
     if (!localThemes) {
-        setToLocalStorage("all-themes", defaultThemes)
+        setToLocalStorage("all-themes", defaultThemes);
     }
 
-    console.log(defaultThemes)
+    console.log(defaultThemes);
 
-    const { theme, themeLoaded } = useTheme()
-    const [selectedTheme, setSelectedTheme] = useState(theme)
+    const { theme, themeLoaded } = useTheme();
+    const [selectedTheme, setSelectedTheme] = useState(theme);
 
     useEffect(() => {
-        setSelectedTheme(theme)
-    }, [theme, themeLoaded])
-    
-    console.log('selectedTheme', selectedTheme)
+        setSelectedTheme(theme);
+    }, [theme, themeLoaded]);
+
+    console.log("selectedTheme", selectedTheme);
 
     return (
         <>
             {themeLoaded && (
                 <GoogleOAuthProvider clientId="970275515164-i13mjqsbta13hg9paobo7bnt51ov4h32.apps.googleusercontent.com">
-                    <ThemeProvider theme={selectedTheme} >
+                    <ThemeProvider theme={selectedTheme}>
                         <GlobalStyles />
                         <StyledApp>
                             <header>
                                 <Nav>
-                                    <ThemePicker themeSetter={setSelectedTheme} />
+                                    <ThemePicker
+                                        themeSetter={setSelectedTheme}
+                                    />
                                 </Nav>
                             </header>
                             <Routes>
-                                <Route path="/" element={ <Login /> }/>
+                                <Route path="/" element={<Login />} />
+                                <Route path="/signup" element={ <Register /> }/>
                             </Routes>
                         </StyledApp>
-                    </ThemeProvider >
+                    </ThemeProvider>
                 </GoogleOAuthProvider>
             )}
         </>
-    )
+    );
 }
 
-export default App
+export default App;
