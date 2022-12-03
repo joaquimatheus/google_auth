91472742c6bf0e2e99fe1e888089f2a4d29b3c08
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-03 20:59:15 -0300

    remake the config in all const

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,12 @@ require("../dotenv.js");
 
 const { DB_DATABASE, DB_PASSWORD, DB_USERNAME, DB_HOST } = process.env;
 
-module.exports = {
-    development: {
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: DB_DATABASE,
-        host: DB_HOST,
-        dialect: "mariadb",
-    },
-};
+const development = {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: "mariadb",
+}
+
+module.exports = { development };
