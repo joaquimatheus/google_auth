9042e3b2e24c4f1c54c4ef215fd581ac660c4b5c
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-03 21:02:05 -0300

    Function the get all models and put in a variable to handle rows

--- server/core/models/index.js
+++ server/core/models/index.js
@@ -1,18 +1,20 @@
-"use strict";
-
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "../../config/config")[env];
+const config = require('../../config/config.js')[env];
 const db = {};
+
+console.log(config)
 
 let sequelize;
 if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    console.log('is here?')
+    sequelize = new Sequelize(`config`, config);
 } else {
+    console.log('is here?--- ')
     sequelize = new Sequelize(
         config.database,
         config.username,
@@ -21,6 +23,8 @@ if (config.use_env_variable) {
     );
 }
 
+
+console.log('arrived here?')
 fs.readdirSync(__dirname)
     .filter((file) => {
         return (
@@ -30,6 +34,7 @@ fs.readdirSync(__dirname)
         );
     })
     .forEach((file) => {
+        console.log('arrived here?------')
         const model = require(path.join(__dirname, file))(
             sequelize,
             Sequelize.DataTypes
