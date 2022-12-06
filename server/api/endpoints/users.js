687aad7479901b70ff707e270566939c2a539f6d
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-06 19:38:23 -0300

    changed the default method to create user by our static method and created a new route to login

--- server/api/endpoints/users.js
+++ server/api/endpoints/users.js
@@ -9,19 +9,27 @@ module.exports = function (app) {
         buildHandler(async function (req, res) {
             const username = req.string('username');
             const email = req.string('email');
-            const password = req.string('email');
+            const password = req.string('password');
 
-            const userRow = await db.users.create({
-                username,
-                email,
-                password
-            })
-
-            console.log(userRow)
+            const userRow = await db.users.createUser(username, email, password)
 
             res.status(200).json({
                 type: 'users',
                 data: userRow
+            })
+        })
+    )
+
+    app.post("/api/v1/users/login",
+        express.json(),
+        buildHandler(async function (req, res) {
+            const email = req.string('email');
+            const password = req.string('password');
+
+            const userRow = await db.users.login(email, password);
+
+            res.status(200).json({
+                user: userRow
             })
         })
     )
