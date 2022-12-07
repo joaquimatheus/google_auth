3bd0aa5c539949f40ea0564c6a8bf45f4d1848b3
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-07 14:20:53 -0300

    used prettier to format the code, and created jwt token when the user will login in the ui

--- server/api/endpoints/users.js
+++ server/api/endpoints/users.js
@@ -1,45 +1,59 @@
-require('../../dotenv.js');
-const express = require('express');
-const { buildHandler } = require('../utils');
-const db = require('../../core/models')
+require("../../dotenv.js");
+const express = require("express");
+const { buildHandler } = require("../utils");
+const db = require("../../core/models");
+const logger = require("../../helpers/logger");
 
+const jwt = require("jsonwebtoken");
 const { HTTP_SECRET } = process.env;
 
 module.exports = function (app) {
-    app.post("/api/v1/users", 
+    app.post(
+        "/api/v1/users",
         express.json(),
         buildHandler(async function (req, res) {
-            const username = req.string('username');
-            const email = req.string('email');
-            const password = req.string('password');
+            const username = req.string("username");
+            const email = req.string("email");
+            const password = req.string("password");
 
-            const userRow = await db.users
-                .createUser(
-                    username, 
-                    email, 
-                    password
+            const userRow = await db.users.createUser(
+                username,
+                email,
+                password
+            );
+
+            if (userRow)
+                logger.info(
+                    `The new user is created! ${userRow.dataValues.id}`
                 );
 
             res.status(200).json({
-                type: 'users',
-                data: userRow
-            })
+                type: "users",
+                data: userRow,
+            });
         })
-    )
+    );
 
-    app.post("/api/v1/users/login",
+    app.post(
+        "/api/v1/users/login",
         express.json(),
         buildHandler(async function (req, res) {
-            const email = req.string('email');
-            const password = req.string('password');
+            const email = req.string("email");
+            const password = req.string("password");
 
             const userRow = await db.users.login(email, password);
-            const token = jwt.sign(user, HTTP_SECRET);
+
+            if (userRow)
+                logger.info(
+                    `The new user is logged ${email}`
+                )
+
+            const token = jwt.sign(userRow.dataValues, HTTP_SECRET);
 
             res.status(200).json({
                 ok: true,
-                token
-            })
+                token,
+            });
         })
-    )
-}
+    );
+};
