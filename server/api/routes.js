c61639a593ea8d98ba45af37dba16991b227c6ad
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-06 21:25:26 -0300

    added more header Authorization for jwt token

--- server/api/routes.js
+++ server/api/routes.js
@@ -2,8 +2,9 @@ function allowCrossDomain(req, res, next) {
 
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Expose-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Expose-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
 
     if (req.method == 'OPTIONS') { return res.sendStatus(200); }
 
