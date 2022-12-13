4bc0947254d13266145b6f01f0245671732fe84a
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-12 21:20:55 -0300

    Used prettier to format and created setNewPassword and validateLoginToken

--- server/core/models/user.js
+++ server/core/models/user.js
@@ -1,41 +1,50 @@
-const bcrypt = require('bcrypt');
-const crypto = require('crypto');
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const { Model } = require("sequelize");
 
 function getGenerateAuthToken() {
     return new Promise((resolve, reject) => {
-        crypto.randomBytes(32, function(err, buf) {
-            if (err) { return reject(err); }
+        crypto.randomBytes(32, function (err, buf) {
+            if (err) {
+                return reject(err);
+            }
 
-            resolve(buf.toString('hex'))
-        })
-    })
+            resolve(buf.toString("hex"));
+        });
+    });
 }
 
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
-
         static async createUser(username, email, password) {
             const salt = await bcrypt.genSalt(10);
 
-            const hashedPassword = await bcrypt.hash(password, salt)
+            const hashedPassword = await bcrypt.hash(password, salt);
 
             const userRow = await User.create({
-                username, email, password: hashedPassword });
+                username,
+                email,
+                password: hashedPassword,
+            });
             return userRow;
         }
 
         static async getUserByEmail(email) {
-            const user = await User.findOne({ where: { email }})
-            if (!user) { throw new Error(`User: ${email} not found!`); }
-            
+            const user = await User.findOne({ where: { email } });
+            if (!user) {
+                throw new Error(`User: ${email} not found!`);
+            }
+
             return user;
         }
 
         static async login(email, password) {
             const user = await this.getUserByEmail(email);
-            const isValidPassword = await bcrypt.compare(password, user.password)
-            
+            const isValidPassword = await bcrypt.compare(
+                password,
+                user.password
+            );
+
             if (!isValidPassword) {
                 throw new Error(`Invalid password for: ${email}`);
             }
@@ -49,20 +58,43 @@ module.exports = (sequelize, DataTypes) => {
 
             const { id } = user;
 
-            await User.update({ login_token: token }, { where: {id} })
+            await User.update({ login_token: token }, { where: { id } });
+
+            // The token is returned by function but
+            // it's only in dev env, it's dangerous if this 
+            // behavior continues in prod env 
+
+            return token;
+        }
+
+        static async validateLoginToken(token) {
+            const user = User.findOne({ where: { login_token: token } });
+
+            if (!user) {
+                throw new Error("Invalid token");
+            }
+        }
+
+        static async setNewPassword(loginToken, password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            return User.update(
+                { password: hashedPassword, login_token: null },
+                { where: { login_token: loginT22oken } }
+            );
         }
 
         static associate(models) {
             // define association here
         }
-
     }
     User.init(
         {
             username: DataTypes.STRING,
             email: DataTypes.STRING,
             password: DataTypes.TEXT,
-            login_token: DataTypes.TEXT
+            login_token: DataTypes.TEXT,
         },
         {
             sequelize,
