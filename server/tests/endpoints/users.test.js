23368973ca8a95253a28b95ad15e85ea06ac7d78
Matheus Zakken <matheus@marcogabriel.com.br>
2022-12-22 20:36:13 -0300

    test updatedUser and shouldLogin

--- server/tests/endpoints/users.test.js
+++ server/tests/endpoints/users.test.js
@@ -49,7 +49,53 @@ describe('User API', () => {
             })
     })
 
-    it('should delete updatedUser', async () => {
-        // TODO TOMORROW! 
+    it('should updatedUser', async () => {
+        const username  = faker.name.firstName().toLowerCase();
+        const email = faker.internet.email(username);
+
+        const data = { "changes": { username, email } };
+        
+        const response = await fetch(`${apiUrl}/api/v1/users`, {
+            method: "GET"
+        }) 
+
+        const users = await response.json();
+        const lastUser = await users.data.pop();
+        const { id } = lastUser;
+
+        return await request(apiUrl)
+            .put(`/api/v1/users/${id}`)
+            .expect(200) 
+            .send(data)
+            .then(res => {
+                expect(res.body).toHaveProperty('data')
+                expect(res.body.data.userId).toEqual(id.toString())
+                expect(res.body.ok).toEqual(true)
+            })
+    })
+
+    it('should login', async () => {
+        const username = faker.name.firstName().toLowerCase();
+        const email    = faker.internet.email(username);
+        const password = faker.internet.password()
+
+        const data = { username, email, password };
+
+        const response = await fetch(`${apiUrl}/api/v1/users`, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        })
+
+        delete data.username;
+
+        return await request(apiUrl)
+            .post(`/api/v1/users/login`)
+            .expect(200)
+            .send(data)
+            .then(res => {
+                expect(res.body).toHaveProperty('token')
+                expect(res.body.ok).toEqual(true)
+            })
     })
 })
